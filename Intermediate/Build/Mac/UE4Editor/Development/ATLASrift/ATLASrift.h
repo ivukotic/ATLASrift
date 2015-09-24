529362ad988c9a44828bef21420311ba825a6a28
Edward Moyse <edward.moyse@gmail.com>
2015-09-24 13:46:01 +0200

    Trying to get cavern switch working

---
+++ Intermediate/Build/Mac/UE4Editor/Development/ATLASrift/ATLASrift.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_ATLASRIFT_H__
+#define __AUTO_ATLASRIFT_H__
+//Last Write: 9/21/2015 9:30:02 AM +00:00
+#include "/Users/emoyse/Documents/Unreal Projects/ATLASrift/Source/ATLASrift/ATLASrift.h"
+#endif//__AUTO_ATLASRIFT_H__
