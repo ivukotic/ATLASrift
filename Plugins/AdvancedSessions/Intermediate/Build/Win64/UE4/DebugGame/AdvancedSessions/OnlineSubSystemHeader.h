327fc09d4a3665d092cbab63a2848f4e66211aeb
Xinran Wang <wangxinran141@163.com>
2016-03-08 23:34:58 -0600

    update .gitignore

--- Plugins/AdvancedSessions/Intermediate/Build/Win64/UE4/DebugGame/AdvancedSessions/OnlineSubSystemHeader.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_ONLINESUBSYSTEMHEADER_H__
-#define __AUTO_ONLINESUBSYSTEMHEADER_H__
-//Last Write: 2/23/2016 3:33:11 PM +00:00
-#include "C:\Users\xinran\Desktop\atlasDownload\ATLASrift\Plugins\AdvancedSessions\Source\AdvancedSessions\Private\OnlineSubSystemHeader.h"
-#endif//__AUTO_ONLINESUBSYSTEMHEADER_H__
