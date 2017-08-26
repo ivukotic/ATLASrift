80d48bc2cff78b799dcd010ea3376fcc869030f5
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2017-08-25 18:59:02 -0700

    Globe and CR are now walking levels. Experiment and Entry will remain flying. Moved posters lower. Fixed initial locations. Change size of the character.

--- Source/ATLASrift.Target.cs
+++ Source/ATLASrift.Target.cs
@@ -5,21 +5,22 @@ using System.Collections.Generic;
 
 public class ATLASriftTarget : TargetRules
 {
-	public ATLASriftTarget(TargetInfo Target)
+	public ATLASriftTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+		ExtraModuleNames.AddRange(new string[] { "ATLASrift" });
 	}
 
 	//
 	// TargetRules interface.
 	//
 
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "ATLASrift" } );
-	}
+	// public override void SetupBinaries(
+	// 	TargetInfo Target,
+	// 	ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
+	// 	ref List<string> OutExtraModuleNames
+	// 	)
+	// {
+	// 	OutExtraModuleNames.AddRange( new string[] { "ATLASrift" } );
+	// }
 }
--- Source/ATLASriftEditor.Target.cs
+++ Source/ATLASriftEditor.Target.cs
@@ -5,21 +5,22 @@ using System.Collections.Generic;
 
 public class ATLASriftEditorTarget : TargetRules
 {
-	public ATLASriftEditorTarget(TargetInfo Target)
+	public ATLASriftEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		ExtraModuleNames.AddRange(new string[] { "ATLASrift" });
 	}
 
 	//
 	// TargetRules interface.
 	//
 
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "ATLASrift" } );
-	}
+	// public override void SetupBinaries(
+	// 	TargetInfo Target,
+	// 	ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
+	// 	ref List<string> OutExtraModuleNames
+	// 	)
+	// {
+	// 	OutExtraModuleNames.AddRange( new string[] { "ATLASrift" } );
+	// }
 }
